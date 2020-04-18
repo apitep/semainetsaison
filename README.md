7c8055eb299be3cec9a55853d85f4d66ef8a46c3
apitep <apitep.factory@gmail.com>
2020-04-19 01:23:15 +0200

    build web app

--- README.md
+++
@@ -1,2 +0,0 @@
-# semainetsaison
-J'apprends les jours de la semaine, les mois et les saisons
--- android/app/src/main/kotlin/com/example/semainetsaison/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.semainetsaison
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
