666d799a8a667f8a223f21c54be61860a04c78f2
apitep <apitep.factory@gmail.com>
2020-06-13 16:52:18 +0200

    fix startup page in landscape

--- lib/screens/startup_screen.dart
+++ lib/screens/startup_screen.dart
@@ -43,95 +43,99 @@ class _StartupScreenState extends State<StartupScreen> with SingleTickerProvider
         init: AppController(),
         initState: (_) {
           AppController.to.init();
-          print(Get.height);
         },
         builder: (_) {
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SizedBox(
-                  height: (Get.height < 400) ? 40 : 60,
-                ),
-                DelayedAnimation(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Image.asset('assets/images/ApitecLogo.png', height: (Get.height < 400) ? 20 : 40),
-                      Text(
-                        "Apprendre à petit pas",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: (Get.height < 400) ? 11 : 14, fontWeight: FontWeight.w300, fontFamily: 'MontserratAlternates', color: Colors.white),
-                      ),
-                    ],
+          return OrientationBuilder(builder: (context, orientation) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    height: (Get.height < 400) ? 40 : 60,
                   ),
-                  delay: delayedAmount + 600,
-                ),
-                SizedBox(height: (Get.height < 400) ? 2 : 15),
-                DelayedAnimation(
-                  child: InkWell(
-                    onTap: () {
-                      if (AppController.to.displayOnBoard) {
-                        Get.toNamed("/onboarding");
-                      } else {
-                        Get.toNamed("/home");
-                      }
-                    },
-                    child: AvatarGlow(
-                      endRadius: (Get.height < 400) ? 60 : 100,
-                      duration: Duration(seconds: 2),
-                      glowColor: Colors.white24,
-                      repeat: true,
-                      repeatPauseDuration: Duration(seconds: 1),
-                      startDelay: Duration(seconds: 1),
-                      child: Material(
-                          elevation: 8.0,
-                          shape: CircleBorder(),
-                          child: CircleAvatar(
-                            backgroundColor: Colors.grey[100],
-                            child: Image.asset('assets/images/ApitepBearLogo.png', height: (Get.height < 400) ? 50 : 90),
-                            radius: (Get.height < 400) ? 40.0 : 70.0,
-                          )),
+                  DelayedAnimation(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Image.asset('assets/images/ApitecLogo.png', height: (Get.height < 400) ? 20 : 40),
+                        Text(
+                          "Apprendre à petit pas",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: (Get.height < 400) ? 11 : 14, fontWeight: FontWeight.w300, fontFamily: 'MontserratAlternates', color: Colors.white),
+                        ),
+                      ],
+                    ),
+                    delay: delayedAmount + 600,
+                  ),
+                  SizedBox(height: (Get.height < 400) ? 2 : 15),
+                  DelayedAnimation(
+                    child: InkWell(
+                      onTap: () {
+                        if (AppController.to.displayOnBoard) {
+                          Get.toNamed("/onboarding");
+                        } else {
+                          Get.toNamed("/home");
+                        }
+                      },
+                      child: AvatarGlow(
+                        endRadius: (Get.height < 400) ? 60 : 100,
+                        duration: Duration(seconds: 2),
+                        glowColor: Colors.white24,
+                        repeat: true,
+                        repeatPauseDuration: Duration(seconds: 1),
+                        startDelay: Duration(seconds: 1),
+                        child: Material(
+                            elevation: 8.0,
+                            shape: CircleBorder(),
+                            child: CircleAvatar(
+                              backgroundColor: Colors.grey[100],
+                              child: Image.asset('assets/images/ApitepBearLogo.png', height: (Get.height < 400) ? 50 : 90),
+                              radius: (Get.height < 400) ? 40.0 : 70.0,
+                            )),
+                      ),
+                    ),
+                    delay: delayedAmount + 200,
+                  ),
+                  SizedBox(height: 5.0),
+                  DelayedAnimation(
+                    child: Text(
+                      Constants.appName,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: (Get.height < 400) ? 13 : 21, fontWeight: FontWeight.w600, fontFamily: 'MontserratAlternates', color: Colors.white),
+                    ),
+                    delay: delayedAmount + 1350,
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(),
+                  ),
+                  SizedBox(height: (Get.height < 400) ? 2 : 15),
+                  DelayedAnimation(
+                    delay: delayedAmount + 1600,
+                    child: Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Image.asset('assets/images/ecoleloisirslogo.png', height: (Get.height < 400) ? 30 : 70),
+                        ),
+                        Center(
+                          child: Text(
+                            "avec les albums animés\noffert par l'école des loisirs",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: (Get.height < 400) ? 11 : 14, fontWeight: FontWeight.w300, fontFamily: 'MontserratAlternates', color: Colors.white),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  delay: delayedAmount + 200,
-                ),
-                SizedBox(height: 5.0),
-                DelayedAnimation(
-                  child: Text(
-                    Constants.appName,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: (Get.height < 400) ? 13 : 21, fontWeight: FontWeight.w600, fontFamily: 'MontserratAlternates', color: Colors.white),
-                  ),
-                  delay: delayedAmount + 1350,
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Container(),
-                ),
-                SizedBox(height: (Get.height < 400) ? 2 : 15),
-                DelayedAnimation(
-                  delay: delayedAmount + 1600,
-                  child: Column(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Image.asset('assets/images/ecoleloisirslogo.png', height: (Get.height < 400) ? 30 : 70),
-                      ),
-                      Center(
-                        child: Text(
-                          "avec les albums animés\noffert par l'école des loisirs",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontSize: (Get.height < 400) ? 11 : 14, fontWeight: FontWeight.w300, fontFamily: 'MontserratAlternates', color: Colors.white),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 20.0),
-              ],
-            ),
-          );
+                  SizedBox(height: 20.0),
+                ],
+              ),
+            );
+          });
         },
       ),
     );
